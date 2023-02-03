85b2543825bfff92f71c278419ff3c746131e05e
Alex Gerber <alex-gerber@mail.ru>
2023-02-03 13:09:37 +0300

    new files

--- src/App.js
+++ src/App.js
@@ -1,81 +1,8 @@
 import { useState } from "react";
 import "./App.css";
 
-const url = "https://api.github.com";
-
-async function fetchResults(query) {
-  try {
-    const response = await fetch(`${url}/search/users?q=${query}`);
-    const json = await response.json();
-    return json.items || [];
-  } catch (e) {
-    throw new Error(e);
-  }
-}
-
-function User({ avatar, url, username }) {
-  return (
-    <div className="user">
-      <img src={avatar} alt="Profile" width="50" height="50" />
-      <a href={url}>{username}</a>
-    </div>
-  );
-}
-
-function Form({ onSubmit, onChange, value }) {
-  return (
-    <form className="search-form" onSubmit={onSubmit}>
-      <input
-        id="search"
-        type="text"
-        placeholder="Enter username or email"
-        onChange={onChange}
-        value={value}
-      />
-      <button type="submit">Search</button>
-    </form>
-  );
-}
-
 function App() {
-  const [query, setQuery] = useState("");
-  const [results, setResults] = useState([]);
-
-  function onSearchChange(event) {
-    setQuery(event.target.value);
-  }
-
-  async function onSearchSubmit(event) {
-    event.preventDefault();
-    const results = await fetchResults(query);
-    setResults(results);
-  }
-
-  return (
-    <div className="app">
-      <main className="main">
-        <h2>GitHub User Search</h2>
-        <Form
-          onChange={onSearchChange}
-          onSubmit={onSearchSubmit}
-          value={query}
-        />
-        <h3>Results</h3>
-        <div id="results">
-          <div>
-            {results.map((user) => (
-              <User
-                key={user.login}
-                avatar={user.avatar_url}
-                url={user.html_url}
-                username={user.login}
-              />
-            ))}
-          </div>
-        </div>
-      </main>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
