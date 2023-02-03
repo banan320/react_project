3ff3d9be7d9e9c9a99bc1acc2a8569626c3fafdd
Alex Gerber <alex-gerber@mail.ru>
2023-02-03 15:09:48 +0300

    video_player

--- src/App.js
+++ src/App.js
@@ -1,8 +1,60 @@
 import { useState } from "react";
 import "./App.css";
 
+function Menu({ onSelectVideo, videoValues }) {
+  return (
+    <form onClick={(event) => onSelectVideo(event.target.value)}>
+      {videoValues.map((value, i) => (
+        <div className="video-inputs">
+          <input key={i} type="radio" name="src" value={value} />
+          {value}
+        </div>
+      ))}
+    </form>
+  );
+}
+
+function Video({ videoSrc }) {
+  return (
+    <div>
+      <video
+        loop
+        controls
+        autostart="true"
+        autoPlay
+        muted
+        src={videoSrc}
+      ></video>
+    </div>
+  );
+}
+
+const videos = {
+  deer: "https://s3.amazonaws.com/codecademy-content/courses/React/react_video-fast.mp4",
+  snail:
+    "https://s3.amazonaws.com/codecademy-content/courses/React/react_video-slow.mp4",
+  cat: "https://s3.amazonaws.com/codecademy-content/courses/React/react_video-cute.mp4",
+  spider:
+    "https://s3.amazonaws.com/codecademy-content/courses/React/react_video-eek.mp4",
+};
+
+const videoNames = Object.keys(videos);
+
 function App() {
-  return <div></div>;
+  const [videoSrc, setVideoSrc] = useState(videos.deer);
+
+  function onSelectVideo(video) {
+    const videoSrc = videos[video];
+    setVideoSrc(videoSrc);
+  }
+
+  return (
+    <div>
+      <h1>Video Player</h1>
+      <Menu onSelectVideo={onSelectVideo} videoValues={videoNames} />
+      <Video videoSrc={videoSrc} />
+    </div>
+  );
 }
 
 export default App;
